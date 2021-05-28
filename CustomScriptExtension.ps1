9508be61cee260731dc2a2fd4ca0a7e4f853ddb2
DarinShapiroMS <dashapir@microsoft.com>
2021-05-28 14:26:27 -0700

    Update CustomScriptExtension.ps1

--- CustomScriptExtension.ps1
+++ CustomScriptExtension.ps1
@@ -7,7 +7,7 @@ $pathOfCurrentScript = Split-Path $MyInvocation.MyCommand.Path -Parent
 $pathOfCreateScript = "$pathOfCurrentScript\CreateStripedLocalDisks.ps1"
 
 # create Windows Scheduled Task to run that script at VM startup
+
 $action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "-File $pathOfCreateScript -NoProfile -WindowStyle Hidden"
 $trigger =  New-ScheduledTaskTrigger -AtStartup
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "fast-temp-disk-create" -Description "creation of striped array of local nvme disks"
-
+Register-ScheduledTask -Action $action -Trigger $trigger -User "System" -RunLevel Highest -TaskName "fast-temp-disk-create" -Description "creation of striped array of local nvme disks"
