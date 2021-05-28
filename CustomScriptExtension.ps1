04c5d7cfbd31e2021b19528d7d6bcae7a32ef90d
DarinShapiroMS <dashapir@microsoft.com>
2021-05-28 13:12:09 -0700

    Update CustomScriptExtension.ps1

--- CustomScriptExtension.ps1
+++ CustomScriptExtension.ps1
@@ -1,16 +1,13 @@
 # this PowerShell script is downloaded to VM by Custom Script Extension.  It's
-# purpose is to download another Powershell script that is run at startup by a 
-# Windows Scheduled Task.
+# purpose is to create a Windows Scheduled Task to run a 2nd PowerShell script 
+# Downloaded but the Custom Script Extension
 
 
-$scriptURL  = 'https://raw.githubusercontent.com/DarinShapiroMS/turbo-octo-barnacle/main/CreateStripedArray.ps1'
-$downloadPath = 'c:\CreateStripedArray.ps1'
-
-# download script to local path
-(New-Object Net.WebClient).DownloadFile($scriptURL,$downloadPath);
+$pathOfCurrentScript = Split-Path $MyInvocation.MyCommand.Path -Parent
+$pathOfCreateScript = "$pathOfCurrentScript\CreateStripedLocalDisks.ps1"
 
 # create Windows Scheduled Task to run that script at VM startup
-
-$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "-File $downloadPath -NoProfile -WindowStyle Hidden"
+$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "-File $pathOfCreateScript -NoProfile -WindowStyle Hidden"
 $trigger =  New-ScheduledTaskTrigger -AtStartup
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "fast-temp-disk-create" -Description "creation of striped array of local nvme disks"
+
